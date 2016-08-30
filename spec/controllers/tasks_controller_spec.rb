dc309348f4d083fd4afc9b6882b95683c761b9ef
Take Mouri <takehiromouri@gmail.com>
2016-08-30 10:08:51 +0545

    add strikethrough

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe TasksController, type: :controller do
       expect(response_value.count).to eq(2)
     end
   end
+
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryGirl.create(:task, done: false)
+      put :update, id: task.id, task: { done: true }
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
+  end
+
+  describe "task#create" do
+    it "should allow new tasks to be created" do
+      post :create, task: {title: "Fix things"}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
 end
